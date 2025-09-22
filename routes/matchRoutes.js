3a78fe762b966a4059d9a0b342892efd501f7fe2
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 19:38:33 +0530

    feat: add route for retrieving active connections

--- routes/matchRoutes.js
+++ routes/matchRoutes.js
@@ -1,29 +1,55 @@
 const express = require("express");
-const { getMatches, createMatch, updateMatch, addPoints, getUpcomingMatches, getPlayersOfMatch, addPointsToOnlyTeam, getMatchScores, getCompletedMatches, getOngoingMatches, getMatchStats, getMatchStatsLive, getMatchTotalPoints, getPointsTable, undoPlayerPoints, undoTeamPoints, setMatchCompleted, markMatchHalfTime, getHalfTimeStatus, setHalfTimeStatus, updateTeamMat, getFinalMatchWinner } = require("../controllers/matchController");
-const { protect, adminOnly } = require("../middleware/authMiddleware");
+const {
+  createMatch,
+  updateMatch,
+  setHalfTimeStatus,
+  updateTeamMat,
+  endMatch,
+  startMatch,
+  getMatch,
+  deleteMatch,
+} = require("../controllers/matchController");
+const {
+  getMatches,
+  getUpcomingMatches,
+  getCompletedMatches,
+  getOngoingMatches,
+  getMatchScores,
+} = require("../controllers/queryController");
+const {
+  getMatchStats,
+  getMatchStatsLive,
+  getMatchTotalPoints,
+  getPointsTable,
+  getFinalMatchWinner,
+  getFullMatchStats,
+} = require("../controllers/statsController");
+
+const { getPlayersOfMatch } = require("../controllers/playerController");
+const { getActiveConnections } = require("../controllers/firebaseController");
 
 const router = express.Router();
 
+router.get("/pointstable", getPointsTable);
+router.get("/final", getFinalMatchWinner);
+router.get("/active-connections", getActiveConnections);
 router.get("/", getMatches);
 router.get("/upcoming", getUpcomingMatches);
 router.get("/completed", getCompletedMatches);
 router.get("/live", getOngoingMatches);
 router.get("/players/:matchId", getPlayersOfMatch);
 router.post("/", createMatch);
-router.put("/:id", updateMatch);
-router.post("/addPoints", addPoints);
-router.post("/addPointstoteam", addPointsToOnlyTeam);
-router.post("/undoPointstoteam", undoTeamPoints);
-router.post("/undoPointstoplayer", undoPlayerPoints);
-router.get("/matchscores/:matchId", getMatchScores);
-router.get("/matchstat/:matchId", getMatchStats);
-router.get("/matchstatlive/:matchId", getMatchStatsLive);
-router.get("/matchstattotal/:matchId", getMatchTotalPoints);
-router.get("/final", getFinalMatchWinner);
-router.get("/pointstable", getPointsTable);
-router.put("/:matchId/complete", setMatchCompleted);
-router.put("/:matchId/halftime", setHalfTimeStatus);
-router.put("/matches/:matchId/teammat", updateTeamMat);
-
+router.patch("/:id", updateMatch);
+router.get("/:matchId", getMatch);
+router.delete("/:matchId", deleteMatch);
+router.patch("/:matchId/start", startMatch);
+router.patch("/:matchId/complete", endMatch);
+router.patch("/:matchId/halftime", setHalfTimeStatus);
+router.patch("/:matchId/teammat", updateTeamMat);
+router.get("/:matchId/scores", getMatchScores); //26 30
+router.get("/:matchId/stats/live", getMatchStatsLive);
+router.get("/:matchId/stats/total", getMatchTotalPoints);
+router.get("/:matchId/stats", getMatchStats);
+router.get("/:matchId/fullstats", getFullMatchStats);
 
 module.exports = router;
