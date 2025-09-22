bd7cb652939cca36c0479fceeccb930816c71b20
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 19:50:00 +0530

    chore: format spacing in stats routes

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { getTopPlayers } = require("../controllers/statsController");
 
 const router = express.Router();
-
 router.get("/top5", getTopPlayers);
 
 module.exports = router;
