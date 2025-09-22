8fc72ae09b6198ab9d9a7b5e4621df64cfcb738b
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 19:33:30 +0530

    feat: add routes for token and refresh token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const { register, login, getProfile } = require("../controllers/authController");
-const { protect, adminOnly } = require("../middleware/authMiddleware");
+const {
+  register,
+  getProfile,
+  getToken,
+  refreshToken,
+} = require("../controllers/authController");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/register", register); // Public
-router.post("/login", login); // Public
-router.get("/profile", protect, getProfile); // Protected route
+router.post("/register", register);
+router.get("/profile", protect, getProfile);
+router.post("/token", getToken);
+router.post("/token/refresh", refreshToken);
 
 module.exports = router;
