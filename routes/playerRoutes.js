2a442ef859f779767f5810bfc592ead665870c56
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 19:46:32 +0530

    chore: add profile pic handling for player in routes

--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const { getPlayers, createPlayer, deletePlayer , bulkPlayer, getPlayersByTeam, setProfilePic, getPlayerDetails} = require("../controllers/playerController");
-
+const {
+  getPlayers,
+  createPlayer,
+  deletePlayer,
+  getPlayersByTeam,
+  getPlayerDetails,
+  updatePlayer,
+  getPlayerById,
+} = require("../controllers/playerController");
 const router = express.Router();
-
+const multer = require("multer");
+const upload = multer({ dest: "tmp/" });
 router.get("/", getPlayers);
-router.get("/playerinfo/:playerId", getPlayerDetails);
-// router.get("/topraiders", getRaiders);
-// router.get("/topdefenders", getDefenders);
-// router.get("/topplayers",getTopPlayers);
-router.get("/:teamId", getPlayersByTeam);
-router.post("/", createPlayer);
+router.get("/:playerId/info", getPlayerDetails);
+router.get("/:playerId", getPlayerById);
+router.get("/team/:teamId", getPlayersByTeam);
+router.post("/", upload.single("profilePic"), createPlayer);
 router.delete("/:id", deletePlayer);
-router.post("/bulk", bulkPlayer);
-
-router.put("/setprofilepic", setProfilePic);
+router.patch("/:id", upload.single("profilePic"), updatePlayer);
 
 module.exports = router;
