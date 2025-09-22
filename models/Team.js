b666366fae96a8159a9431add63d6e9600244595
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 18:25:43 +0530

    feat: add pre-delete hook to Team model for match and player cleanup

--- models/Team.js
+++ models/Team.js
@@ -1,11 +1,29 @@
 const mongoose = require("mongoose");
-
+const Player = require('./Player');
+const Match = require('./Match');
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  logo: { type: String }, // URL to team logo
-  players: [{ type: mongoose.Schema.Types.ObjectId, ref: "Player" }], // Reference to players
+  logo: { type: String }, 
   createdAt: { type: Date, default: Date.now },
 });
+
+teamSchema.pre("deleteOne", { document: true, query: false }, async function (next) {
+  try {
+    const matchCount = await Match.countDocuments({
+      $or: [{ teamA: this._id }, { teamB: this._id }],
+    });
+
+    if (matchCount > 0) {
+      return next(new Error("Cannot delete team: team is assigned to matches."));
+    }
+
+    await Player.deleteMany({ team: this._id });
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 const Team = mongoose.model("Team", teamSchema);
 module.exports = Team;
