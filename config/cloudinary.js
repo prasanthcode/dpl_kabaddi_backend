140fc32fc272b71f127f9fa549039e1778d27ad3
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 18:14:22 +0530

    refactor: simplify Cloudinary configuration by using a single environment variable

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,11 +1,5 @@
 const cloudinary = require("cloudinary").v2;
-require("dotenv").config(); // Load .env variables
-
-// Configure Cloudinary
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
+require("dotenv").config();
+cloudinary.config(process.env.CLOUDINARY_URL);
 
 module.exports = cloudinary;
